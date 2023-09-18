1c2df6f97172d13d057e34317fb1771f1a185d70
Guilherme M. Xavier <100965793+GuilhermeMX@users.noreply.github.com>
2023-09-17 23:34:36 -0300

    create useBalance hook

--- src/components/Balance/index.tsx
+++ src/components/Balance/index.tsx
@@ -1,25 +1,11 @@
 import { ArrowCircleUp, CurrencyDollar } from "phosphor-react";
-import { useContext } from "react";
-
-import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 import { BalanceCard, BalanceContainer } from "./styles";
 import { priceFormatter } from "../../utils/formatter";
+import { useBalance } from "../../hooks/useBalance";
 
 export function Balance() {
-  const { transactions } = useContext(TransactionsContext)
-
-  const balance = transactions.reduce((acc, transaction) => {
-    if (transaction.type === 'income') {
-      acc.income += transaction.price;
-      acc.total += transaction.price;
-    } else {
-      acc.outcome += transaction.price;
-      acc.total -= transaction.price;
-    }
-
-    return acc;
-  }, { income: 0, outcome: 0, total: 0 })
+  const balance = useBalance()
 
   return (
     <BalanceContainer>
